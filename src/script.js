d8190d6352c9bbdc4ee737dcc25657b702065fd2
Marie-Aimee-N <marieaimeendiho@gmail.com>
2023-09-06 17:34:15 +0200

    update in JS

--- src/script.js
+++ src/script.js
@@ -1,68 +1,118 @@
-let dateElement = document.querySelector("#date");
-let now = new Date();
-let hours = now.getHours();
-if (hours < 10) {
-  hours = `0${hours}`;
+function formatDate(timestamp) {
+  let dt = new Date(timestamp);
+  let hours = dt.getHours();
+  let minutes = dt.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[dt.getDay()];
+  return `${day} ${hours}:${minutes}`;
 }
 
-let minutes = now.getMinutes();
-if (minutes < 10) {
-  minutes = `0${minutes}`;
+function formatdays(timestep) {
+  let date = new Date(timestep * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+  return days[day];
 }
-let dayIndex = now.getDay();
+function displayForecast(response) {
+  let daysdata = response.data.daily;
+  let forecast = document.querySelector("#weather-forecast");
+  let forecasthtml = `<div class="row">`;
 
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
+  daysdata.forEach(function (forecastday, index) {
+    if (index < 6) {
+      forecasthtml =
+        forecasthtml +
+        `
+                        <div class="col-2">
+                            <div class="weather-forecast-date">${formatdays(
+                              forecastday.dt
+                            )}</div>
+                            <img src="http://openweathermap.org/img/wn/${
+                              forecastday.weather[0].icon
+                            }@2x.png" alt="" width="42" />
+                            <div class="weather-forecast-temperatures">
+                                <span class="weather-forecast-temperature-max"> ${Math.round(
+                                  forecastday.temp.max
+                                )}° </span>
+                                <span class="weather-forecast-temperature-min"> ${Math.round(
+                                  forecastday.temp.min
+                                )}° </span>
+                            </div>        
+                    </div>`;
+    }
+  });
 
-dateElement.innerHTML = `${days[dayIndex]} ${hours}:${minutes}`;
+  forecasthtml = forecasthtml + `</div >`;
+  forecast.innerHTML = forecasthtml;
+}
+function getforecast(coordinate) {
+  let api = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinate.lat}&lon=${coordinate.lon}&units=metric&appid=72bb9dab46b9ec3d65f423c63f27a9b8`;
+  axios.get(api).then(displayForecast);
+}
+function displayTemperature(response) {
+  let temp = document.querySelector("#temperature");
+  let city = document.querySelector("#city");
+  let description = document.querySelector("#description");
+  let humidity = document.querySelector("#humidity");
+  let wind = document.querySelector("#wind");
+  let date = document.querySelector("#date");
+  let icon = document.querySelector("#icon");
+  cili = Math.round(response.data.main.temp);
 
-function weatherCondition(response) {
-  document.querySelector("#city").innerHTML = response.data.name;
-  document.querySelector("#temperature").innerHTML = Math.round(
-    response.data.main.temp
+  temp.innerHTML = Math.round(cili);
+  city.innerHTML = response.data.name;
+  description.innerHTML = response.data.weather[0].description;
+  humidity.innerHTML = response.data.main.humidity;
+  wind.innerHTML = response.data.wind.speed;
+  date.innerHTML = formatDate(response.data.dt * 1000);
+  getforecast(response.data.coord);
+  icon.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
-  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
-  document.querySelector("#wind").innerHTML = Math.round(
-    response.data.wind.speed
-  );
-  document.querySelector("#description").innerHTML =
-    response.data.weather[0].main;
 }
 
 function search(city) {
-  let apiKey = "b2ca82f87efeafa1149cbcf2389b8c3a";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(weatherCondition);
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=72bb9dab46b9ec3d65f423c63f27a9b8`;
+  axios.get(apiUrl).then(displayTemperature);
 }
 
-function handleSubmit(event) {
+function handdle(event) {
   event.preventDefault();
-  let city = document.querySelector("#result-city").value;
-  search(city);
+  let cityinput = document.querySelector("#city-input");
+  search(cityinput.value);
 }
-
-function getCurrentLocation(event) {
+function turnfaren(event) {
   event.preventDefault();
-  navigator.geolocation.getCurrentPosition(searchLocation);
+  let temp1 = document.querySelector("#temperature");
+  sili.classList.remove("active");
+  faren.classList.add("active");
+  let farentemp = (cili * 9) / 5 + 32;
+  temp1.innerHTML = Math.round(farentemp);
 }
-
-function searchLocation(position) {
-  let apiKey = "ece50fe34649f448476ecda477cfcc21";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(weatherCondition);
+let cili = null;
+function turnsili(event) {
+  event.preventDefault();
+  let temp2 = document.querySelector("#temperature");
+  sili.classList.add("active");
+  faren.classList.remove("active");
+  temp2.innerHTML = cili;
 }
+search("sydney");
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handdle);
 
-let searchCity = document.querySelector("#search-city");
-searchCity.addEventListener("submit", handleSubmit);
+let faren = document.querySelector("#fahrenheit-link");
+faren.addEventListener("click", turnfaren);
+let sili = document.querySelector("#celsius-link");
+sili.addEventListener("click", turnsili);
 
-let currentLocationButton = document.querySelector("#current-location");
-currentLocationButton.addEventListener("click", getCurrentLocation);
-
-search("Mbabane");
+displayForecast();
